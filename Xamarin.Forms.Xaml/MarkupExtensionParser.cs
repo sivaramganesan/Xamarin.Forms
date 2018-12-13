4e112a86b135c1c89f524b6e190e472cac5d1195
Stephane Delcroix <stephane@delcroix.org>
2018-12-13 11:52:42 +0100

    [Xaml] require escaping curly braces
    This is a breaking change as we weren't throwing errors on that before.
    
    - fixes #2756

--- Xamarin.Forms.Xaml/MarkupExtensionParser.cs
+++ Xamarin.Forms.Xaml/MarkupExtensionParser.cs
@@ -5,27 +5,31 @@ namespace Xamarin.Forms.Xaml
 {
 	internal sealed class MarkupExtensionParser : MarkupExpressionParser, IExpressionParser<object>
 	{
-		IMarkupExtension markupExtension;
+		IMarkupExtension _markupExtension;
 
 		public object Parse(string match, ref string remaining, IServiceProvider serviceProvider)
 		{
 			var typeResolver = serviceProvider.GetService(typeof (IXamlTypeResolver)) as IXamlTypeResolver;
-
-			//shortcut for Binding and StaticResource, to avoid too many reflection calls.
-			if (match == "Binding")
-				markupExtension = new BindingExtension();
-			else if (match == "TemplateBinding")
-				markupExtension = new TemplateBindingExtension();
-			else if (match == "StaticResource")
-				markupExtension = new StaticResourceExtension();
-			else if (match == "OnPlatform")
-				markupExtension = new OnPlatformExtension();
-			else if (match == "OnIdiom")
-				markupExtension = new OnIdiomExtension();
-			else if (match == "DataTemplate")
-				markupExtension = new DataTemplateExtension();
-			else
-			{
+			switch (match) {
+			case "Binding":
+				_markupExtension = new BindingExtension();
+				break;
+			case "TemplateBinding":
+				_markupExtension = new TemplateBindingExtension();
+				break;
+			case "StaticResource":
+				_markupExtension = new StaticResourceExtension();
+				break;
+			case "OnPlatform":
+				_markupExtension = new OnPlatformExtension();
+				break;
+			case "OnIdiom":
+				_markupExtension = new OnIdiomExtension();
+				break;
+			case "DataTemplate":
+				_markupExtension = new DataTemplateExtension();
+				break;
+			default:
 				if (typeResolver == null)
 					return null;
 				Type type;
@@ -37,10 +41,11 @@ namespace Xamarin.Forms.Xaml
 					var lineInfo = (lineInfoProvider != null) ? lineInfoProvider.XmlLineInfo : new XmlLineInfo();
 					throw new XamlParseException(String.Format("MarkupExtension not found for {0}", match), lineInfo);
 				}
-				markupExtension = Activator.CreateInstance(type) as IMarkupExtension;
+				_markupExtension = Activator.CreateInstance(type) as IMarkupExtension;
+				break;
 			}
 
-			if (markupExtension == null)
+			if (_markupExtension == null)
 			{
 				var lineInfoProvider = serviceProvider.GetService(typeof (IXmlLineInfoProvider)) as IXmlLineInfoProvider;
 				var lineInfo = (lineInfoProvider != null) ? lineInfoProvider.XmlLineInfo : new XmlLineInfo();
@@ -50,13 +55,13 @@ namespace Xamarin.Forms.Xaml
 
 			char next;
 			if (remaining == "}")
-				return markupExtension.ProvideValue(serviceProvider);
+				return _markupExtension.ProvideValue(serviceProvider);
 
 			string piece;
 			while ((piece = GetNextPiece(ref remaining, out next)) != null)
 				HandleProperty(piece, serviceProvider, ref remaining, next != '=');
 
-			return markupExtension.ProvideValue(serviceProvider);
+			return _markupExtension.ProvideValue(serviceProvider);
 		}
 
 		protected override void SetPropertyValue(string prop, string strValue, object value, IServiceProvider serviceProvider)
@@ -65,7 +70,7 @@ namespace Xamarin.Forms.Xaml
 			if (prop == null)
 			{
 				//implicit property
-				var t = markupExtension.GetType();
+				var t = _markupExtension.GetType();
 				prop = ApplyPropertiesVisitor.GetContentPropertyName(t.GetTypeInfo());
 				if (prop == null)
 					return;
@@ -79,25 +84,25 @@ namespace Xamarin.Forms.Xaml
 			else {
 				try
 				{
-					setter = markupExtension.GetType().GetRuntimeProperty(prop).SetMethod;
+					setter = _markupExtension.GetType().GetRuntimeProperty(prop).SetMethod;
 				} catch (AmbiguousMatchException e) {
 					var lineInfo = (serviceProvider.GetService(typeof(IXmlLineInfoProvider)) is IXmlLineInfoProvider lineInfoProvider) ? lineInfoProvider.XmlLineInfo : new XmlLineInfo();
-					throw new XamlParseException($"Multiple properties with name  '{markupExtension.GetType()}.{prop}' found.", lineInfo, innerException: e);
+					throw new XamlParseException($"Multiple properties with name  '{_markupExtension.GetType()}.{prop}' found.", lineInfo, innerException: e);
 				}
 
 			}
 			if (value == null && strValue != null)
 			{
 				try {
-					value = strValue.ConvertTo(markupExtension.GetType().GetRuntimeProperty(prop).PropertyType,
+					value = strValue.ConvertTo(_markupExtension.GetType().GetRuntimeProperty(prop).PropertyType,
 						(Func<TypeConverter>)null, serviceProvider);
 				} catch (AmbiguousMatchException e) {
 					var lineInfo = (serviceProvider.GetService(typeof(IXmlLineInfoProvider)) is IXmlLineInfoProvider lineInfoProvider) ? lineInfoProvider.XmlLineInfo : new XmlLineInfo();
-					throw new XamlParseException($"Multiple properties with name  '{markupExtension.GetType()}.{prop}' found.", lineInfo, innerException: e);
+					throw new XamlParseException($"Multiple properties with name  '{_markupExtension.GetType()}.{prop}' found.", lineInfo, innerException: e);
 				}
 			}
 
-			setter.Invoke(markupExtension, new[] { value });
+			setter.Invoke(_markupExtension, new[] { value });
 		}
 	}
 }
